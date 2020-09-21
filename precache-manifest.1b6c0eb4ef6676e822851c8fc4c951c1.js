61c56c1fdf99a55598782097397748bf50384ff9
Ibrahim <alaoopeyemi101@gmail.com>
2020-09-21 14:49:39 +0100

    Updates

---
+++ precache-manifest.1b6c0eb4ef6676e822851c8fc4c951c1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097dd2346d6452dbfcfff89bc848c4a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a774fa06b4e0a5312602",
+    "url": "/static/css/main.d28d94b2.chunk.css"
+  },
+  {
+    "revision": "098ed9ffc696f47243af",
+    "url": "/static/js/2.4a0ab242.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.4a0ab242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a774fa06b4e0a5312602",
+    "url": "/static/js/main.1e8fc48a.chunk.js"
+  },
+  {
+    "revision": "ecfdfd6e38dd3aa77c08",
+    "url": "/static/js/runtime-main.6b84a375.js"
+  },
+  {
+    "revision": "73c563f6ebe2eb700940cc6ed10c8b25",
+    "url": "/static/media/Ellipse 3wil.73c563f6.png"
+  },
+  {
+    "revision": "a16d6ef738021031131700d4cba08898",
+    "url": "/static/media/Ellipse3.a16d6ef7.png"
+  },
+  {
+    "revision": "c07488f68b3dcea74254c24aaf5437f9",
+    "url": "/static/media/Rectangle4.c07488f6.png"
+  },
+  {
+    "revision": "fca4e6a4373fc030692e56d489aec593",
+    "url": "/static/media/User.fca4e6a4.svg"
+  },
+  {
+    "revision": "db5397afa1a7af39deae97b639e70a7f",
+    "url": "/static/media/cheerful-woman.db5397af.png"
+  },
+  {
+    "revision": "cdfd4250798eba662562f7134be8d9c9",
+    "url": "/static/media/remi-landingpage-background.cdfd4250.png"
+  },
+  {
+    "revision": "631efe8213d69b446e7a698f61d8e847",
+    "url": "/static/media/woman_overlay.631efe82.png"
+  }
+]);
